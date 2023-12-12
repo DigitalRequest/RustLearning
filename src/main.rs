19aaa3ed3a200000eb6572e50f77c26ded82e293
DigitalRequest <DigitalRequest@github.com>
2023-12-12 08:57:11 -0300

    Added a new function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 use std::time::{Duration, Instant};
+use std::collections::HashMap;
+use rand::Rng;
 
 mod functions;
 
 fn main() {
+    test_fun();
+}
+
+fn sum_array_singles(arr: Vec<i32>) -> i32 {
+    let mut dict: HashMap<_, _> = HashMap::new();
+    let mut sum: i32 = 0;
+
+    for x in &arr {
+        let value = if dict.contains_key(&x) {
+            2
+        } else {
+            1
+        };
+
+        dict.insert(x, value);
+    }
+
+    for x in arr.iter() {
+        if let Some(&value) = dict.get(&x) {
+            if value == 1 {
+                sum += x;
+            }
+        }
+    }
+
+    sum
+}
+
+fn test_fun() {
     let num_instances = 20;
     let mut total_time = Duration::default();
 
@@ -10,24 +41,16 @@ fn main() {
         let mut input = Vec::new();
 
         for _ in 0..100000 {
-            let mut word = String::new();
-            for _ in 0..50 {
-                word.push('a');
-                word.push('b');
-                word.push('c');
-            }
-            input.push(word);
+            input.push(rand::thread_rng().gen_range(1..=10000));
         }
 
         let start_time = Instant::now();
-        let result = functions::remove_consec_dup_optimized(input);
+        let result = sum_array_singles(input);
         let end_time = Instant::now();
 
         total_time += end_time - start_time;
 
-        // Imprima apenas uma parte do resultado para evitar uma saída muito longa
-        println!("{:?}", &result[0..10]);
-        println!("Total de strings no resultado: {}", result.len());
+        println!("Result: {}", result)
     }
 
     let average_time = total_time / num_instances as u32;
